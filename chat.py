57b7e10fffced52c372d03fc4721a6a30f8a27d5
HasseenSayed <hasseen012@gmail.com>
2024-03-05 22:44:54 +0100

    Added server.py

--- chat.py
+++ chat.py
@@ -1,4 +1,6 @@
 import socket
 
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-    s.sendto(b"hello", ("192.168.1.10", 5678))
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect(("192.168.1.10", 5678))
+    s.sendall("Hii :3")
+    print(s.recv(1024))
