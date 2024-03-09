0631ec83b0da043783ae87ecb48735c7cfbffea3
HasseenSayed <hasseen012@gmail.com>
2024-03-09 23:27:52 +0100

    Added handle_client

--- server.py
+++ server.py
@@ -1,14 +1,18 @@
 import socket
 
+def handle_client(conn, addr):
+    while True:
+        data = conn.recv(1024)
+        conn.sendall(b"Received")
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((socket.gethostname(), 5678))
     s.listen()
     conn, addr = s.accept()
-    with conn:
-        print(f"Connected with {addr}")
+    handle_client(conn, addr)
 
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall("Yo bro\n")
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        conn.sendall("Yo bro\n")
